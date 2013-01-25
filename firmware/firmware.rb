ac6d4a3d6c6beb0895eba5e94cd921a3e3d6e701
Ron <ron@skullsecurity.net>
2013-01-25 14:39:02 -0600

    Fixed the extracted files - they all had one extra byte - and extracted the gzipped ones

--- firmware/firmware.rb
+++ firmware/firmware.rb
@@ -19,29 +19,19 @@ offset = mpfs_start + MPFS_HEADER_SIZE
 0.upto(31) do |i|
   name_offset, data_offset, data_length, checksum, u1, u2, name_end, table_offset, table_length, checksum_again, u3, u4 = f[offset..-1].unpack("IIIIISIIIIIS")
   name = f[(mpfs_start + name_offset)..-1].unpack("Z*").pop
-  data = f[(mpfs_start + data_offset)..(mpfs_start + data_offset + data_length)]
+  data = f[(mpfs_start + data_offset)..(mpfs_start + data_offset + data_length - 1)]
   if(name == '')
     name = "unnamed_index_#{i}"
   end
 
   puts("Name offset: 0x%08x: %s" % [name_offset, name])
-  puts("Data:")
-  puts(data)
-  w = File.open(name, "w")
+  puts("Data offset: 0x%08x" % [data_offset])
+  #puts("Data:")
+  #puts(data)
+  w = File.open("extracted/#{name}", "w")
   w.write(data)
   w.close
 
-  #puts("Name: 0x%08x: %s" % [name_offset, name])
-  #puts("Data: 0x%08x" % data_offset)
-  #puts("Len:  0x%08x" % length)
-  #puts("Unknown: 0x%08x" % u1)
-  #puts("Unknown: 0x%08x" % u2)
-  #puts("Unknown: 0x%08x" % u3)
-  #puts("Unknown: 0x%08x" % u4)
-  #puts("Unknown: 0x%08x" % u5)
-  #puts("Unknown: 0x%08x" % u6)
-  #puts("Unknown: 0x%08x" % u7)
-  #puts("Unknown: 0x%08x" % u8)
   puts()
   offset += MPFS_ENTRY_SIZE
 end
